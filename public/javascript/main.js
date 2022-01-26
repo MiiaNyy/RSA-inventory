08fac803d8c6b28cafb939983814812e114869a8
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2022-01-26 16:08:36 +0200

    final styles added

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -7,6 +7,8 @@ function toggleDropDown (e) {
 function openSidebar () {
     moveNavAndMainContentToRight(200);
     document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("sidebar__content").style.display = 'block';
+    document.getElementById("sidebar__content").style.minHeight = '95vh';
     document.getElementById("sidebar__content").style.visibility = "visible";
     document.getElementById("open-sidebar__btn").style.display = 'none';
 }
@@ -15,6 +17,7 @@ function closeSidebar () {
     moveNavAndMainContentToRight(50);
     document.getElementById("sidebar").style.width = screen.width <= 600 ? "40px" : "50px";
     document.getElementById("sidebar__content").style.visibility = "hidden";
+    document.getElementById("sidebar__content").style.minHeight = '0';
     document.getElementById("open-sidebar__btn").style.display = "block";
 }
 
